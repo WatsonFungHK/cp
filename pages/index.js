9fb0603fb41674ff74ebf35cce646c75e3d90630
WatsonFungHK <watson.cwfung@gmail.com>
2023-07-11 10:07:48 +0800

    add next head for webpage title

--- pages/index.js
+++ pages/index.js
@@ -9,31 +9,36 @@ import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <Box
-      sx={{
-        boxSizing: "border-box",
-      }}
-    >
-      <Header />
+    <>
+      <Head>
+        <title>Choco Up: Growth capital for fast-growing businesses</title>
+      </Head>
       <Box
         sx={{
-          backgroundImage: `url(https://uploads-ssl.webflow.com/63f6b93e52c07002355ba424/641313aea9b46d2d4b83979b_Mesh%20gradient%201.svg),url(https://uploads-ssl.webflow.com/63f6b93e52c07002355ba424/63f6bebdf7a7562fe6c2075d_Group%202358.svg)`,
-          backgroundPosition: "100% 0,0 0",
-          backgroundRepeat: "no-repeat,no-repeat",
-          backgroundSize: "auto,auto",
-          "> *": {
-            p: "80px 80px 0",
-            margin: "0 auto",
-            boxSizing: "border-box",
-          },
+          boxSizing: "border-box",
         }}
       >
-        <AboutTheFold />
-        <UnlockBusiness />
-        <SuccessStory />
-        <Testimonials />
+        <Header />
+        <Box
+          sx={{
+            backgroundImage: `url(https://uploads-ssl.webflow.com/63f6b93e52c07002355ba424/641313aea9b46d2d4b83979b_Mesh%20gradient%201.svg),url(https://uploads-ssl.webflow.com/63f6b93e52c07002355ba424/63f6bebdf7a7562fe6c2075d_Group%202358.svg)`,
+            backgroundPosition: "100% 0,0 0",
+            backgroundRepeat: "no-repeat,no-repeat",
+            backgroundSize: "auto,auto",
+            "> *": {
+              p: "80px 80px 0",
+              margin: "0 auto",
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <AboutTheFold />
+          <UnlockBusiness />
+          <SuccessStory />
+          <Testimonials />
+        </Box>
+        <Footer />
       </Box>
-      <Footer />
-    </Box>
+    </>
   );
 }
